fcf9e08070edc03076fdc54c59eafd74be7a47e8
LAKSHYA UPADDHYAY <95523785+laxupaddhyay@users.noreply.github.com>
2024-04-13 19:43:55 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/3D-art-gallery',
+  base: '/3d-art-gallery',
   build: {
     rollupOptions: {
       input: '/main.js',
